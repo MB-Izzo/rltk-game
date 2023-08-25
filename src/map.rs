1a9ed87ca6dc1c3b9d2b80fb9e61318c9d348963
mbouchonnet <mathieubouchonnet@gmail.com>
2023-08-25 20:37:42 +0200

    better impl of visibility, remember what was seen, faster

--- src/map.rs
+++ src/map.rs
@@ -17,6 +17,7 @@ pub struct Map {
     pub rooms: Vec<Rect>,
     pub width: i32,
     pub height: i32,
+    pub revealed_tiles: Vec<bool>,
 }
 
 impl Algorithm2D for Map {
@@ -42,6 +43,7 @@ impl Map {
             rooms: Vec::new(),
             width: 80,
             height: 80,
+            revealed_tiles: vec![false; 80 * 50],
         };
 
         const MAX_ROOMS: i32 = 30;
@@ -114,45 +116,40 @@ impl Map {
 }
 
 pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
-    let mut viewsheds = ecs.write_storage::<Viewshed>();
-    let mut players = ecs.write_storage::<Player>();
     let map = ecs.fetch::<Map>();
 
-    for (_player, viewshed) in (&mut players, &mut viewsheds).join() {
-        let mut y = 0;
-        let mut x = 0;
+    let mut y = 0;
+    let mut x = 0;
 
-        for tile in map.tiles.iter() {
-            // Render a tile depending upon the tile type
-            let pt = Point::new(x, y);
-            if viewshed.visible_tiles.contains(&pt) {
-                match tile {
-                    TileType::Floor => {
-                        ctx.set(
-                            x,
-                            y,
-                            RGB::from_f32(0.5, 0.5, 0.5),
-                            RGB::from_f32(0., 0., 0.),
-                            rltk::to_cp437('.'),
-                        );
-                    }
-                    TileType::Wall => {
-                        ctx.set(
-                            x,
-                            y,
-                            RGB::from_f32(0.0, 1.0, 0.0),
-                            RGB::from_f32(0., 0., 0.),
-                            rltk::to_cp437('#'),
-                        );
-                    }
+    for (idx, tile) in map.tiles.iter().enumerate() {
+        // Render a tile depending upon the tile type
+        if map.revealed_tiles[idx] {
+            match tile {
+                TileType::Floor => {
+                    ctx.set(
+                        x,
+                        y,
+                        RGB::from_f32(0.5, 0.5, 0.5),
+                        RGB::from_f32(0., 0., 0.),
+                        rltk::to_cp437('.'),
+                    );
+                }
+                TileType::Wall => {
+                    ctx.set(
+                        x,
+                        y,
+                        RGB::from_f32(0.0, 1.0, 0.0),
+                        RGB::from_f32(0., 0., 0.),
+                        rltk::to_cp437('#'),
+                    );
                 }
             }
-            // move coords
-            x += 1;
-            if x > 79 {
-                x = 0;
-                y += 1;
-            }
+        }
+        // move coords
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
         }
     }
 }
--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -1,21 +1,32 @@
 use specs::prelude::*;
-use super::{Viewshed, Position, Map};
+use super::{Viewshed, Position, Map, Player};
 use rltk::{field_of_view, Point};
 
 pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
-    type SystemData = (ReadExpect<'a, Map>,
+    type SystemData = (WriteExpect<'a, Map>,
+                       Entities<'a>,
                         WriteStorage<'a, Viewshed>,
-                       WriteStorage<'a, Position>);
+                       WriteStorage<'a, Position>,
+                       ReadStorage<'a, Player>);
 
     fn run(&mut self, data : Self::SystemData) {
-        let (map, mut viewshed, pos) = data;
+        let (mut map, entities, mut viewshed, pos, player) = data;
 
-        for (viewshed, pos) in (&mut viewshed, &pos).join() {
+        for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
             viewshed.visible_tiles.clear();
             viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
             viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >=0 && p.y < map.height);
+
+            // runs only if entity is Player
+            let player: Option<&Player> = player.get(ent);
+            if let Some(_player) = player { 
+                for visible_tile in viewshed.visible_tiles.iter() {
+                    let idx = map.get_index_at(visible_tile.x, visible_tile.y);
+                    map.revealed_tiles[idx] = true;
+                }
+            }
         }
     }
 }
