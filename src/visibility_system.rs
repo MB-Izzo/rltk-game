856e6a5b05365c445c04b319903ac38223a7914a
mbouchonnet <mathieubouchonnet@gmail.com>
2023-08-25 20:45:09 +0200

    added a dirty flag to update visible tiles more efficiently

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -8,23 +8,25 @@ impl<'a> System<'a> for VisibilitySystem {
     type SystemData = (WriteExpect<'a, Map>,
                        Entities<'a>,
                         WriteStorage<'a, Viewshed>,
-                       WriteStorage<'a, Position>,
+                       ReadStorage<'a, Position>,
                        ReadStorage<'a, Player>);
 
     fn run(&mut self, data : Self::SystemData) {
         let (mut map, entities, mut viewshed, pos, player) = data;
-
         for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
-            viewshed.visible_tiles.clear();
-            viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
-            viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >=0 && p.y < map.height);
+            if viewshed.dirty {
+                viewshed.dirty = false;
+                viewshed.visible_tiles.clear();
+                viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+                viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >=0 && p.y < map.height);
 
-            // runs only if entity is Player
-            let player: Option<&Player> = player.get(ent);
-            if let Some(_player) = player { 
-                for visible_tile in viewshed.visible_tiles.iter() {
-                    let idx = map.get_index_at(visible_tile.x, visible_tile.y);
-                    map.revealed_tiles[idx] = true;
+                // runs only if entity is Player
+                let player: Option<&Player> = player.get(ent);
+                if let Some(_player) = player { 
+                    for visible_tile in viewshed.visible_tiles.iter() {
+                        let idx = map.get_index_at(visible_tile.x, visible_tile.y);
+                        map.revealed_tiles[idx] = true;
+                    }
                 }
             }
         }
