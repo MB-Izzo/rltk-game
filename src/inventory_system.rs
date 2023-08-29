68444d27ab94d3f8c4bcf896259e32a5031b6c7c
mbouchonnet <mathieubouchonnet@gmail.com>
2023-08-29 23:04:11 +0200

    bugfix: confusion scrolls removes from inventory after being used

--- src/inventory_system.rs
+++ src/inventory_system.rs
@@ -233,6 +233,7 @@ impl<'a> System<'a> for ItemUseSystem {
                                 ));
                             }
                         }
+                        used_item = true;
                     }
                 }
             }
